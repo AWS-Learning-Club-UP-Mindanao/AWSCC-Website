b0d30f964dfcbe43842a348c6c1df9e8f628bc40
kindadailybren <bomedina1@up.edu.ph>
2025-07-22 20:51:08 +0800

    refactor: initiateStaticNavBar to be reusable

--- assets/js/navbar-static.js
+++ assets/js/navbar-static.js
@@ -1,4 +1,29 @@
-export const initiateStaticNavBar = () => {
+export const initiateStaticNavBar = (container) => {
+  // Fetch and insert navbar
+  fetch("../assets/components/navbar.html")
+    .then((res) => res.text())
+    .then((html) => {
+      container.innerHTML = html;
+
+      addStaticNavClass();
+
+      const dropdown = document.querySelectorAll(".navbar-dropdown-button");
+      const navbar = document.querySelector(".navbar");
+
+      // Dropdown toggle logic
+      if (dropdown && navbar) {
+        dropdown.forEach((button) => {
+          button.addEventListener("click", () => {
+            navbar.classList.toggle("dropdown");
+            navbar.parentElement.classList.toggle("dropdown");
+            document.body.classList.toggle("dropdown");
+          });
+        });
+      }
+    })
+}
+
+const addStaticNavClass = () => {
   const navbarBG = document.querySelector(".navbar-wrapper");
   const logo = document.querySelector(".navbar-logo");
   const text1 = document.querySelector(".navbar-logo-text.top");
@@ -17,4 +42,5 @@ export const initiateStaticNavBar = () => {
   text1.classList.add(scrolled);
   text2.classList.add(scrolled);
   navbarBG.classList.add(blurredNav);
+
 }
